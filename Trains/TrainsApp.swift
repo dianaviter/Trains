9484fbea7f8ebfd414dcebfd2e065306c129b0b4
Diana Viter <diviterok@gmail.com>
2025-07-07 23:21:35 +0300

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Diana Viter on 07.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrainsTests/TrainsTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsTests.swift
+//  TrainsTests
+//
+//  Created by Diana Viter on 07.07.2025.
+//
+
+import Testing
+@testable import Trains
+
+struct TrainsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
