9553c281d9f8ecebd51784cc5f6b8cbd39096c81
Diana Viter <diviterok@gmail.com>
2025-07-13 00:03:49 +0300

    добавлен ключ API Яндекс Расписаний, код сетевого слоя для 8 сетевых сервисов API Яндекс Расписаний, код с примером вызова каждого из сервисов

--- Services/RouteStationsService.swift
+++ Services/RouteStationsService.swift
@@ -5,3 +5,30 @@
 //  Created by Diana Viter on 10.07.2025.
 //
 
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+typealias RouteStations = Components.Schemas.ThreadStationsResponse
+
+protocol RouteStationsProtocol {
+    func getRouteStations(uid: String) async throws -> RouteStations
+}
+
+
+final class RouteStationsService: RouteStationsProtocol {
+
+    private let client: Client
+    private let apikey: String
+    
+    init(client: Client, apikey: String) {
+        self.client = client
+        self.apikey = apikey
+    }
+    
+    func getRouteStations(uid: String) async throws -> RouteStations {
+        let response = try await client.getRouteStations(query: .init(
+            apikey: apikey,
+            uid: uid))
+        return try response.ok.body.json
+    }
+}
